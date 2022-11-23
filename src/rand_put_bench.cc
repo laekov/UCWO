b0dd73f1bed0174be0f7bd1972170d049b06faf5
Rick Ho <laekov.h@gmail.com>
2022-11-23 16:47:56 +0800

    rand put bench fixed

--- src/rand_put_bench.cc
+++ src/rand_put_bench.cc
@@ -1,4 +1,4 @@
-#include "ucxctrl.hh"
+#include "ucwo.hh"
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -9,9 +9,10 @@
 #include <omp.h>
 #include "timer.hh"
 
-const size_t n = 1 << 22;
+const size_t n = 1 << 20;
 const size_t bs = 64;
-const size_t nt = 100;
+const size_t nt = 10;
+const int nth = 4;
 
 int main() {
     MPI_Init(0, 0);
@@ -20,38 +21,24 @@ int main() {
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     assert(world_size == 2);
 
-    fprintf(stderr, "Rank %d init\n", rank);
-    ucxCtrl::init();
-    fprintf(stderr, "Rank %d connect\n", rank);
-    ucxCtrl::connect();
-    fprintf(stderr, "Rank %d connected\n", rank);
+    UCWO::World world(MPI_COMM_WORLD);
+    std::vector<UCWO::Worker*> workers;
+    for (int i = 0; i < nth; ++i) {
+        workers.push_back(world.newWorker());
+    }
 
     void* addr = 0;
     size_t len = n * sizeof(int);
     if (rank == 0) {
-        auto rkb = ucxCtrl::mmap(addr, len);
-        int* ptr = (int*)addr;
-        ucxCtrl::exposeMemory(rkb, addr, 1);
+        addr = world.expose(addr, len);
+        MPI_Barrier(MPI_COMM_WORLD);
 
-        int join_worker = 0;
-        std::vector<std::thread> ths;
-        for (int i = 0; i < ucxCtrl::nth; ++i) {
-            ths.emplace_back([&join_worker, i]() {
-                while (!join_worker) {
-                    ucxCtrl::yield(i);
-                }
-            });
-        }
+        int* ptr = (int*)addr;
         for (size_t i = 0; i < nt; ++i) {
             MPI_Barrier(MPI_COMM_WORLD);
             fprintf(stderr, "Put %d output %x\n", i, ptr[0]);
         }
-        join_worker = 1;
-        for (auto& th: ths) {
-            th.join();
-        }
     } else {
-        auto rm = ucxCtrl::peepMemory(0);
         int* p = new int[n * nt];
         memset(p, 12, n * nt * sizeof(int));
         for (int i = 0; i < nt; ++i) {
@@ -63,30 +50,36 @@ int main() {
         }
         std::random_shuffle(x, x + n / bs);
 
+        MPI_Barrier(MPI_COMM_WORLD);
         double tott = 0;
         fprintf(stderr, "Starting bench\n");
         for (size_t i = 0; i < nt; ++i) {
             timestamp(tb);
-#pragma omp parallel for num_threads(ucxCtrl::nth)
+#pragma omp parallel for num_threads(nth)
             for (int j = 0; j < n / bs; ++j) {
                 int thi = omp_get_thread_num();
-                ucxCtrl::putAsync(0, rm[thi], x[j] * bs * sizeof(int),
-                        p + i * n + j * bs, bs * sizeof(int));
+                workers[thi]->wait(workers[thi]->put(0, 0,
+                            x[j] * bs * sizeof(int),
+                            p + i * n + j * bs, bs * sizeof(int)));
             }
-            ucxCtrl::flush();
+            for (auto& w: workers) {
+                w->flush();
+            }
             timestamp(te);
 
             MPI_Barrier(MPI_COMM_WORLD);
             auto dur = getDuration(tb, te);
             auto bw = n * sizeof(int) / dur;
-            fprintf(stderr, "Put %d time %.3lf ms bw %.3lf GBps\n", i, dur * 1e3, bw * 1e-9);
+            fprintf(stderr, "Put %d time %.3lf ms bw %.3lf GBps\n",
+                    i, dur * 1e3, bw * 1e-9);
             if (i) {
                 tott += dur;
             }
         }
         auto meant = tott / (nt - 1);
         auto bw = n * sizeof(int) / meant;
-        fprintf(stderr, "Mean time %.3lf bw %.3lf GBps\n", meant * 1e3, bw * 1e-9);
+        fprintf(stderr, "Mean time %.3lf bw %.3lf GBps\n",
+                meant * 1e3, bw * 1e-9);
     }
     sleep(1);
     MPI_Finalize();
